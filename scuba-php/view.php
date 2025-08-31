3fdb8dfca684f392e8cbb80ec0e66ba86eb9b20d
Willian <willian.rodrigues@winker.com.br>
2025-08-31 15:21:09 -0300

    feat: update controller & view

--- scuba-php/view.php
+++ scuba-php/view.php
@@ -1,6 +1,6 @@
 <?php
 
 function render_view($template) {
-    $content = file_get_contents(VIEW_FOLDER . $template);
+    $content = file_get_contents(VIEW_FOLDER . "$template.view");
     echo $content;
 }
